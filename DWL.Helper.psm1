a7cb73e3beda1e700e46ea5ed01f2a9cefe9292f
Devin Leaman <dleaman212@gmail.com>
2016-12-19 02:29:00 -0800

    Initial Commit

---
+++ DWL.Helper.psm1
@@ -0,0 +1,63 @@
+﻿<#	
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2016 v5.3.130
+	 Created on:   	11/10/2016 10:49
+	 Created by:   	DevinL
+	 Organization: 	SAPIEN Technologies, Inc.
+	 Filename:     	DWL.Helper.psm1
+	-------------------------------------------------------------------------
+	 Module Name: DWL.Helper
+	===========================================================================
+#>
+
+function Get-RandomString {
+	param
+	(
+		[Parameter(Mandatory = $false,
+				   Position = 0)]
+		[int]
+		$CharacterCount = 10
+	)
+	
+	$RandomString = New-Object -TypeName System.Text.StringBuilder
+	
+	while ($RandomString.Length -le $CharacterCount) {
+		[char]$RandomChar = (65 .. 90) + (97 .. 122) | Get-Random -Count 1
+		[void]$RandomString.Append($RandomChar)
+	}
+	
+	$RandomString.ToString()
+}
+
+function Increment-iPowerShellVersion {
+	param
+	(
+		[Parameter(Mandatory = $true,
+				   Position = 0)]
+		[System.Version]
+		$IncrementValue
+	)
+	
+	$StringFile = "C:\Development\Android\Projects\iPowerShell_Android\app\src\main\res\values\strings.xml"
+	$GradleFile = "C:\Development\Android\Projects\iPowerShell_Android\gradle.properties"
+	
+	$StringFileContent = Get-Content $StringFile
+	$GradleFileContent = Get-Content $GradleFile
+	
+	$CurrentVersion = $GradleFileContent | Select-Object -last 1 | ForEach-Object { ($_ -split '=')[-1] }
+	$CurrentVersion = [System.Version]::New($CurrentVersion)
+	
+	$NewVersion = [System.Version]::New(($CurrentVersion.Major + $IncrementValue.Major), `
+		($CurrentVersion.Minor + $IncrementValue.Minor), `
+		($CurrentVersion.Build + $IncrementValue.Build))
+	
+	$StringFileContent -replace "$CurrentVersion", "$NewVersion" | Set-Content $StringFile
+	$GradleFileContent -replace "$CurrentVersion", "$NewVersion" | Set-Content $GradleFile
+	
+	$ProjectDir = Split-Path -Path $GradleFile
+	
+	&git -C $ProjectDir add .
+	&git -C $ProjectDir commit -m "Version increased."
+}
+
+Get-ChildItem -Path $PSScriptRoot -Filter '*.ps1' -Recurse | ForEach-Object { . $_.FullName }
---
+++ Get-MortyPictures.ps1
@@ -0,0 +1,90 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2016 v5.3.130
+	 Created on:   	11/19/2016 10:07 AM
+	 Created by:   	DevinL
+	 Organization: 	SAPIEN Technologies, Inc.
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+function Get-FrontMortyImages {
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Position = 0)]
+		[String]
+		$StorageDir = "D:\Media\Pictures\Morty\"
+	)
+	
+	$WebClient = New-Object -TypeName System.Net.WebClient
+	
+	for ($x = 2; $x -le 168; $x++) {
+		$MortyCounter = "{0:D3}" -f $x
+		$Url = "https://pocketmortys.net/images/large/$MortyCounter-Front.png"
+		
+		$FileName = "$StorageDir\Morty-$MortyCounter-Front.png"
+		if ($null -eq (Get-ChildItem $FileName)) {
+			$WebClient.DownloadFile($Url, $FileName)
+		}
+	}
+}
+
+function Get-BackMortyImages {
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Position = 0)]
+		[String]
+		$StorageDir = "D:\Media\Pictures\Morty\"
+	)
+	
+	$WebClient = New-Object -TypeName System.Net.WebClient
+	
+	for ($x = 2; $x -le 168; $x++) {
+		$MortyCounter = "{0:D3}" -f $x
+		$Url = "https://pocketmortys.net/images/large/$MortyCounter-Back.png"
+		
+		$FileName = "$StorageDir\Morty-$MortyCounter-Back.png"
+		if ($null -eq (Get-ChildItem $FileName)) {
+			$WebClient.DownloadFile($Url, $FileName)
+		}
+	}
+}
+
+function Get-MortySprites {
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Position = 0)]
+		[String]
+		$StorageDir = "D:\Media\Pictures\Morty\"
+	)
+	
+	$WebClient = New-Object -TypeName System.Net.WebClient
+	
+	for ($x = 2; $x -le 168; $x++) {
+		$MortyCounter = "{0:D3}" -f $x
+		
+		for ($y = 1; $y -le 3; $y++) {
+			[System.String]$Url = "https://pocketmortys.net/images/sprites/sprites_" + "$MortyCounter" + "_" + $y + ".png"
+			[System.String]$FileName = "$StorageDir\Sprite_" + "$MortyCounter" + "_" + $y + ".png"
+			if ($null -eq (Get-ChildItem $FileName)) {
+				$WebClient.DownloadFile($Url, $FileName)
+			}
+		}
+	}
+}
+
+function Get-AllMortyImages {
+	Write-Output "Getting front images..."
+	Get-FrontMortyImages
+	
+	Write-Output "Getting back images..."
+	Get-BackMortyImages
+	
+	Write-Output "Getting sprite images..."
+	Get-MortySprites
+}
